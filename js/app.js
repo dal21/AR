961972e786f7e95cbff863fa65c033871477c170
dal21 <38189650+dal21@users.noreply.github.com>
2021-03-30 14:24:44 +0100

    Third Upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/NuttyTest2019(12).json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/NuttyTest2019(13).json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
